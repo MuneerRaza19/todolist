db45480fb0285a805e6203d754aac46ed61c3449
Muneer-Raza Attari <131713767+MuneerRaza19@users.noreply.github.com>
2023-11-10 22:25:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const taskInput = document.getElementById('task');
-  const addTaskButton = document.getElementById('addTask');
-  const taskList = document.getElementById('taskList');
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
 
-  addTaskButton.addEventListener('click', function () {
-    const taskText = taskInput.value.trim();
-    if (taskText !== '') {
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `
-        ${taskText}
-        <button class="delete">Delete</button>
-      `;
-      taskList.appendChild(listItem);
-      taskInput.value = '';
+function inputLength(){
+	return input.value.length;
+} 
 
-      // Add event listener to delete button
-      const deleteButton = listItem.querySelector('.delete');
-      deleteButton.addEventListener('click', function () {
-        listItem.remove();
-      });
-    }
-  });
-});
+function listLength(){
+	return item.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
+
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
+
+
+	// START ADD DELETE BUTTON
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	// END ADD DELETE BUTTON
+
+
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+}
+
+
+function addListAfterClick(){
+	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
+		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+		createListElement();
+	} 
+}
+
+
+enterButton.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
